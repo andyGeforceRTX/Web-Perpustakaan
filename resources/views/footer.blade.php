bb33164f80793c7947785d60de8573d0d669160b
DESKTOP-V0I874V\HP <21053007@student.uwp.ac.id>
2024-05-18 18:33:47 +0700

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,27 @@
+<p>
+    <!--awal FOOTER-->
+    <div class="col-md-12">
+        <div class="row" style="background:rgba(63, 67, 102, 0.562)">
+            <div class="col-md-9">
+                <h3>Visi</h3>
+                <p>Terwujudnya Indonesia PIntar berwawasan tinggi dengan gelora semangat membaca buku
+
+                <p>
+                <h3>Misi</h3>
+                <ul>
+                    <li>Terwujudnya Manusia Pintar dan Berkompeten</li>
+                    <li>Terwujudnya Perpustakaan sebagai pelestari khasanah budaya bangsa dan Negara (MERDEKA)</li>
+                    <li>Terwujudnya Perpustakaan abal-abal sesuai standar internasional</li>
+                </ul>                     
+            </div>
+            <div class="col-md-3">
+                Copyright &copy; 2023. All right reserve
+                <p>
+                Developed by Andylaok</div>
+        </div>
+    </div>
+    <!--akhir FOOTER-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+       
---
+++ resources/views/view_home.blade.php
@@ -0,0 +1,44 @@
+@extends('index')
+@section('title', 'Home')
+
+@section('isihalaman')
+    <h3>Perpustakaan Abal-Abal Internasional</h3>
+    <p>
+    <h4>PERPUSTAKAAN</h4>
+    Perpustakaan adalah mencakup suatu ruangan, bagian dari gedung / bangunan atau gedung tersendiri yang berisi bukubuku koleksi, 
+    yang diatur dan disusun demikian rupa, sehingga mudah untuk dicari dan dipergunakan apabila sewaktu-waktu diperlukan oleh pembaca (Sutarno NS, 2006:11).
+    <p>
+    <h4>Fungsi Perpustakan Abal-Abal Andy</h4>
+    <p>
+    <h6>1. Fungsi Edukatif</h6>
+    Perpustakaan berfungsi sebagai tempat untuk belajar secara mandiri, di situ
+    pengguna dapat mencari bahan-bahan yang dibutuhkan untuk menambah ilmu
+    dan wawasan.
+    </p>  
+    <h6>2. Fungsi Informatif.</h6>
+    <p>
+    Perpustakaan mempunyai fungsi informatif, artinya informasi yang dibutuhkan
+    oleh pengguna dapat dicari di perpustakaan
+    </p> 
+    <h6>3. Fungsi Penelitian</h6>
+    <p>
+    Perpustakaan mempunyai fungsi penelitian, artinya, sumber-sumber informasi
+    yang ada di dalam perpustakaan tersebut dapat digunakan sebagai bahan rujukan
+    untuk melakukan penelitian.
+
+    <p>
+        Dengan adanya fungsi perpustakaan yang beragam tersebut, diharapkan para pengguna akan tertarik, sehingga sebanyak dan sesering mungkin datang ke
+        perpustakaan. Perpustakaan harus mampu menjembatani kepentingan berbagai pihak. Para pustakawan dan pengelola perpustakaan seyogyanya menyediakan fasilitas dan
+        koleksi bahan pustaka yang memadai dan relevan bagi penggunanya.
+    <p>
+    <h4>Undang-Undang 43 Tahun 2007 Tentang Perpustakaan</h4>
+    <p>
+    Pengaturan Standar Perpustakaan bertujuan untuk:
+    <P> 
+    a. Perpustakaan adalah institusi pengelola koleksi karya tulis, karya cetak, dan/atau karya rekam secara profesional dengan sistem yang baku guna memenuhi kebutuhan pendidikan, penelitian, pelestarian, informasi, dan rekreasi para pemustaka.
+    <p>
+    b. perpustakaan merupakan wahana pelestarian kekayaan budaya bangsa, dan dalam rangka meningkatkan kecerdasan kehidupan bangsa.
+    <p>
+    c. perlu ditumbuhkan budaya gemar membaca melalui pengembangan dan pendayagunaan perpustakaan sebagai sumber informasi yang berupa karya tulis, karya cetak, dan/atau karya rekam.
+
+@endsection
